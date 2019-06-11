9dc0b5cb74ea5af567301731e74cc3e584940e7f
Ryan Block <rblock@gmail.com>
2019-06-11 12:36:06 -0700

    Get rid of unnecessary mocks

--- test/mock/src/http/get-api/index.js
+++
@@ -1,8 +0,0 @@
-let data = require('@architect/data')
-
-exports.handler = async function http(req) {
-  let results = await data.cats.scan({})
-  return {
-    body: JSON.stringify(results)
-  }  
-}
--- test/mock/src/http/get-health/index.js
+++
@@ -1,5 +0,0 @@
-exports.handler = async function http(req) {
-  return {
-    body: JSON.stringify({ok:true})
-  }  
-}
