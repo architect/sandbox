3cd97ce598502a9491f815c2244cee06f3e9a586
Ryan Block <rblock@gmail.com>
2019-06-24 07:40:23 -0700

    CLI wrapper now properly callbacky, adds CLI test

--- src/cli.js
+++ src/cli.js
@@ -3,4 +3,9 @@ let sandbox = require('./index')
 let ver = require('../package.json').version
 let version = `Sandbox ${ver}`
 let options = process.argv
-sandbox.start({version, options})
+sandbox.start({version, options}, function _done(err) {
+  if (err) {
+    console.log(err)
+    process.exit(1)
+  }
+})
--- test/start-test.js
+++ test/start-test.js
@@ -1,41 +1,60 @@
 let path = require('path')
 let test = require('tape')
+let spawn = require('child_process').spawn
 let sandbox = require('../src')
 
 test('sandbox.start', t=> {
   t.plan(2)
-  t.ok(sandbox, 'has sandbox')
-  t.ok(sandbox.start, 'has sandbox.start')
+  t.ok(sandbox, 'Has sandbox')
+  t.ok(sandbox.start, 'Has sandbox.start')
 })
 
 let asyncClose
-test('async sandbox.start test/mock', async t=> {
+test('Async sandbox.start test/mock', async t=> {
   t.plan(1)
   process.chdir(path.join(__dirname, 'mock'))
   asyncClose = await sandbox.start()
-  t.ok(asyncClose, 'started')
+  t.ok(asyncClose, 'Sandbox started (async)')
 })
 
-test('async sandbox.close', async t=> {
+test('Async sandbox.close', async t=> {
   t.plan(1)
   asyncClose()
-  t.ok(true, 'closed')
+  t.ok(true, 'Sandbox closed')
 })
 
 let syncClose
-test('sync sandbox.start test/mock', t=> {
+test('Sync sandbox.start test/mock', t=> {
   t.plan(1)
   sandbox.start({}, function (err, end) {
-    if (err) t.fail('Sandbox startup failure')
+    if (err) t.fail('Sandbox failed (sync)')
     else {
       syncClose = end
-      t.ok(syncClose, 'started')
+      t.ok(syncClose, 'Sandbox started (sync)')
     }
   })
 })
 
-test('sync sandbox.close', t=> {
+test('Sync sandbox.close', t=> {
   t.plan(1)
   syncClose()
-  t.ok(true, 'closed')
+  t.ok(true, 'Sandbox closed')
+})
+
+test('CLI sandbox', t => {
+  t.plan(1)
+  let result = spawn('../../src/cli.js')
+  let output = ''
+  result.stdout.on('data', (data) => {
+    output += data
+    if (output.includes(`Started HTTP "server"`)) {
+      console.log(output)
+      result.kill('SIGINT')
+      t.ok(true, 'Sandbox CLI started')
+    }
+  })
+  result.on('error', err => {
+    console.log('', err)
+    t.fail()
+  })
 })
