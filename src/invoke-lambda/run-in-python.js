a8c02f42b471cee41b42de54f52a4e28fdbb9953
Ryan Block <rblock@gmail.com>
2019-07-22 14:51:29 -0700

    Adds check for appropriate python executable in Windows

--- src/invoke-lambda/run-in-python.js
+++ src/invoke-lambda/run-in-python.js
@@ -4,11 +4,12 @@ let spawn = require('./spawn')
 
 module.exports = function runInPython(options, timeout, callback) {
   let python = path.join(__dirname, 'runtimes', 'python.py')
+  let py = process.platform === 'win32' ? 'python.exe' : 'python3'
   fs.readFile(python, 'utf8', function done(err, data) {
     if (err) callback(err)
     else {
       let script = `"${data.toString()}"`
-      spawn('python3', ['-c', script], options, timeout, callback)
+      spawn(py, ['-c', script], options, timeout, callback)
     }
   })
 }
