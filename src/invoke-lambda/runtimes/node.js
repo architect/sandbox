909c6eadd5c54f8459b8b576285d4a164e174fe5
Ryan Block <rblock@gmail.com>
2019-07-22 10:45:59 -0700

    Updates legacy node context to new mock context obj

--- src/invoke-lambda/runtimes/node.js
+++ src/invoke-lambda/runtimes/node.js
@@ -1,19 +1,11 @@
 let fn = require('./index').handler;
 let event = JSON.parse(process.env.__ARC_REQ__);
+let context = process.env.__ARC_CONTEXT__;
 
 function callback(err, result) {
   let payload = err? {name:err.name, message:err.message, stack:err.stack} : result;
   console.log('__ARC__', JSON.stringify(payload));
 }
-
-var context = {
-  succeed(x) {
-    callback(null, x)
-  },
-  fail(x) {
-    callback(x)
-  }
-};
 
 if (fn.constructor.name === 'AsyncFunction') {
   fn(event, context, callback).then(function win(result) {
