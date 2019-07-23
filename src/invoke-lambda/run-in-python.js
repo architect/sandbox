e5f1df5200a02df09ce77cbf8b0d16e51b406651
Ryan Block <rblock@gmail.com>
2019-07-22 18:40:00 -0700

    Dart-throwing should now be over. Thanks Windows and/or Python's impl of python -c + multi-line scripts!

--- src/invoke-lambda/run-in-python.js
+++ src/invoke-lambda/run-in-python.js
@@ -5,13 +5,11 @@ let spawn = require('./spawn')
 module.exports = function runInPython(options, timeout, callback) {
   let python = path.join(__dirname, 'runtimes', 'python.py')
   let py = process.platform === 'win32' ? 'python' : 'python3'
-  // TODO ↓ remove me! ↓
-  console.log(`process.platform === 'win32'`, process.platform === 'win32')
-  console.log(`py`, py)
   fs.readFile(python, 'utf8', function done(err, data) {
     if (err) callback(err)
     else {
-      let script = `"${data.toString()}"`
+      data = data.toString().split('\n').filter(l => l !== '').join(';')
+      let script = `"${data}"`
       spawn(py, ['-c', script], options, timeout, callback)
     }
   })
