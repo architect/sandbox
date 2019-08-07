3c55576839f340a6a41cea5dcfa28dd96a31bf63
Ryan Block <rblock@gmail.com>
2019-08-07 14:20:09 -0700

    Adds Architect 5 + 6 request tests

---
+++ test/unit/src/http/invoke-http-req-test.js
@@ -0,0 +1,480 @@
+let test = require('tape')
+let sinon = require('sinon')
+let proxyquire = require('proxyquire')
+let lambdaStub = sinon.stub().yields()
+let invoke = proxyquire('../../../../src/http/invoke-http', {
+  '../invoke-lambda': lambdaStub
+})
+let reqs = require('./http-req-fixtures')
+
+lambdaStub.yields(null, {})
+let headers = {'Accept-Encoding': 'gzip'}
+let url = i => `http://localhost:3333${i ? i : ''}`
+let str = i => JSON.stringify(i)
+let match = (copy, item) => `${copy} matches: ${str(item)}`
+let response = {
+  setHeader: sinon.fake.returns(),
+  end: sinon.fake.returns()
+}
+let teardown = () => {
+  lambdaStub.reset()
+  delete process.env.ARC_CFN
+}
+
+test('Architect v6: get /', t => {
+  t.plan(6)
+  let request = reqs.arc6.getIndex
+  process.env.ARC_CFN = true
+  let verb = 'GET'
+  let route = '/'
+  let handler = invoke({verb, route})
+  let input = {
+    url: url(), // Set by `router` (interpolated, API passes path param)
+    body: {},   // {} set by `body-parser` (Arc 5 == {}, Arc 6 == null)
+    headers,    // Set by requesting client
+    params: {}  // {} set by `router` (Arc 5 == {}, Arc 6 == null)
+  }
+  handler(input, response)
+  // Compare handler-generated request to mock
+  let req = lambdaStub.args[0][1]
+  t.equal(str(request.body), str(req.body), match('req.body', req.body))
+  t.equal(str(request.path), str(req.path), match('req.path', req.path))
+  t.equal(str(request.headers), str(req.headers), match(`req.headers`, req.headers))
+  t.equal(str(request.httpMethod), str(req.httpMethod), match('req.httpMethod', req.httpMethod))
+  t.equal(str(request.pathParameters), str(req.pathParameters), match('req.pathParameters', req.pathParameters))
+  t.equal(str(req.queryStringParameters), str(req.queryStringParameters), match('req.queryStringParameters', req.queryStringParameters))
+  teardown()
+})
+
+test('Architect v6: get /?whats=up', t => {
+  t.plan(6)
+  let request = reqs.arc6.getWithQueryString
+  process.env.ARC_CFN = true
+  let verb = 'GET'
+  let route = '/'
+  let handler = invoke({verb, route})
+  let input = {
+    url: url('?whats=up'),
+    body: {},
+    headers,
+    params: {}
+  }
+  handler(input, response)
+  let req = lambdaStub.args[0][1]
+  t.equal(str(request.body), str(req.body), match('req.body', req.body))
+  t.equal(str(request.path), str(req.path), match('req.path', req.path))
+  t.equal(str(request.headers), str(req.headers), match(`req.headers`, req.headers))
+  t.equal(str(request.httpMethod), str(req.httpMethod), match('req.httpMethod', req.httpMethod))
+  t.equal(str(request.pathParameters), str(req.pathParameters), match('req.pathParameters', req.pathParameters))
+  t.equal(str(req.queryStringParameters), str(req.queryStringParameters), match('req.queryStringParameters', req.queryStringParameters))
+  teardown()
+})
+
+test('Architect v6: get /nature/hiking', t => {
+  t.plan(7)
+  let request = reqs.arc6.getWithParam
+  process.env.ARC_CFN = true
+  let verb = 'GET'
+  let route = '/nature/:activities'
+  let handler = invoke({verb, route})
+  let input = {
+    url: url('/nature/hiking'),
+    body: {},
+    headers,
+    params: request.pathParameters
+  }
+  handler(input, response)
+  let req = lambdaStub.args[0][1]
+  t.equal(str(request.body), str(req.body), match('req.body', req.body))
+  t.equal(str(request.path), str(req.path), match('req.path', req.path))
+  t.equal(str(request.resource), str(req.resource), match('req.resource', req.resource))
+  t.equal(str(request.headers), str(req.headers), match(`req.headers`, req.headers))
+  t.equal(str(request.httpMethod), str(req.httpMethod), match('req.httpMethod', req.httpMethod))
+  t.equal(str(request.pathParameters), str(req.pathParameters), match('req.pathParameters', req.pathParameters))
+  t.equal(str(req.queryStringParameters), str(req.queryStringParameters), match('req.queryStringParameters', req.queryStringParameters))
+  teardown()
+})
+
+test('Architect v6: post /form (JSON)', t => {
+  t.plan(7)
+  let request = reqs.arc6.postJson
+  process.env.ARC_CFN = true
+  let verb = 'POST'
+  let route = '/form'
+  let handler = invoke({verb, route})
+  let input = {
+    url: url('/form'),
+    body: request.body,
+    headers: request.headers,
+    params: {},
+    isBase64Encoded: true // Assumes flag is set in binary handler
+  }
+  handler(input, response)
+  let req = lambdaStub.args[0][1]
+  t.equal(str(request.body), str(req.body), match('req.body', req.body))
+  t.equal(str(request.path), str(req.path), match('req.path', req.path))
+  t.equal(str(request.headers), str(req.headers), match(`req.headers`, req.headers))
+  t.equal(str(request.httpMethod), str(req.httpMethod), match('req.httpMethod', req.httpMethod))
+  t.equal(str(request.pathParameters), str(req.pathParameters), match('req.pathParameters', req.pathParameters))
+  t.equal(str(req.queryStringParameters), str(req.queryStringParameters), match('req.queryStringParameters', req.queryStringParameters))
+  lambdaStub.reset()
+  t.ok(req.isBase64Encoded, 'req.isBase64Encoded present')
+  delete process.env.ARC_CFN
+})
+
+test('Architect v6: post /form (form URL encoded)', t => {
+  t.plan(7)
+  let request = reqs.arc6.postFormURL
+  process.env.ARC_CFN = true
+  let verb = 'POST'
+  let route = '/form'
+  let handler = invoke({verb, route})
+  let input = {
+    url: url('/form'),
+    body: request.body,
+    headers: request.headers,
+    params: {},
+    isBase64Encoded: true
+  }
+  handler(input, response)
+  let req = lambdaStub.args[0][1]
+  t.equal(str(request.body), str(req.body), match('req.body', req.body))
+  t.equal(str(request.path), str(req.path), match('req.path', req.path))
+  t.equal(str(request.headers), str(req.headers), match(`req.headers`, req.headers))
+  t.equal(str(request.httpMethod), str(req.httpMethod), match('req.httpMethod', req.httpMethod))
+  t.equal(str(request.pathParameters), str(req.pathParameters), match('req.pathParameters', req.pathParameters))
+  t.equal(str(req.queryStringParameters), str(req.queryStringParameters), match('req.queryStringParameters', req.queryStringParameters))
+  t.ok(req.isBase64Encoded, 'req.isBase64Encoded present')
+  teardown()
+})
+
+test('Architect v6: post /form (multipart form data)', t => {
+  t.plan(7)
+  let request = reqs.arc6.postMultiPartFormData
+  process.env.ARC_CFN = true
+  let verb = 'POST'
+  let route = '/form'
+  let handler = invoke({verb, route})
+  let input = {
+    url: url('/form'),
+    body: request.body,
+    headers: request.headers,
+    params: {},
+    isBase64Encoded: true
+  }
+  handler(input, response)
+  let req = lambdaStub.args[0][1]
+  t.equal(str(request.body), str(req.body), match('req.body', req.body))
+  t.equal(str(request.path), str(req.path), match('req.path', req.path))
+  t.equal(str(request.headers), str(req.headers), match(`req.headers`, req.headers))
+  t.equal(str(request.httpMethod), str(req.httpMethod), match('req.httpMethod', req.httpMethod))
+  t.equal(str(request.pathParameters), str(req.pathParameters), match('req.pathParameters', req.pathParameters))
+  t.equal(str(req.queryStringParameters), str(req.queryStringParameters), match('req.queryStringParameters', req.queryStringParameters))
+  t.ok(req.isBase64Encoded, 'req.isBase64Encoded present')
+  teardown()
+})
+
+test('Architect v6: post /form (octet stream)', t => {
+  t.plan(7)
+  let request = reqs.arc6.postOctetStream
+  process.env.ARC_CFN = true
+  let verb = 'POST'
+  let route = '/form'
+  let handler = invoke({verb, route})
+  let input = {
+    url: url('/form'),
+    body: request.body,
+    headers: request.headers,
+    params: {},
+    isBase64Encoded: true
+  }
+  handler(input, response)
+  let req = lambdaStub.args[0][1]
+  t.equal(str(request.body), str(req.body), match('req.body', req.body))
+  t.equal(str(request.path), str(req.path), match('req.path', req.path))
+  t.equal(str(request.headers), str(req.headers), match(`req.headers`, req.headers))
+  t.equal(str(request.httpMethod), str(req.httpMethod), match('req.httpMethod', req.httpMethod))
+  t.equal(str(request.pathParameters), str(req.pathParameters), match('req.pathParameters', req.pathParameters))
+  t.equal(str(req.queryStringParameters), str(req.queryStringParameters), match('req.queryStringParameters', req.queryStringParameters))
+  t.ok(req.isBase64Encoded, 'req.isBase64Encoded present')
+  teardown()
+})
+
+test('Architect v6: put /form (JSON)', t => {
+  t.plan(7)
+  let request = reqs.arc6.putJson
+  process.env.ARC_CFN = true
+  let verb = 'PUT'
+  let route = '/form'
+  let handler = invoke({verb, route})
+  let input = {
+    url: url('/form'),
+    body: request.body,
+    headers: request.headers,
+    params: {},
+    isBase64Encoded: true
+  }
+  handler(input, response)
+  let req = lambdaStub.args[0][1]
+  t.equal(str(request.body), str(req.body), match('req.body', req.body))
+  t.equal(str(request.path), str(req.path), match('req.path', req.path))
+  t.equal(str(request.headers), str(req.headers), match(`req.headers`, req.headers))
+  t.equal(str(request.httpMethod), str(req.httpMethod), match('req.httpMethod', req.httpMethod))
+  t.equal(str(request.pathParameters), str(req.pathParameters), match('req.pathParameters', req.pathParameters))
+  t.equal(str(req.queryStringParameters), str(req.queryStringParameters), match('req.queryStringParameters', req.queryStringParameters))
+  t.ok(req.isBase64Encoded, 'req.isBase64Encoded present')
+  teardown()
+})
+
+test('Architect v6: patch /form (JSON)', t => {
+  t.plan(7)
+  let request = reqs.arc6.patchJson
+  process.env.ARC_CFN = true
+  let verb = 'PATCH'
+  let route = '/form'
+  let handler = invoke({verb, route})
+  let input = {
+    url: url('/form'),
+    body: request.body,
+    headers: request.headers,
+    params: {},
+    isBase64Encoded: true
+  }
+  handler(input, response)
+  let req = lambdaStub.args[0][1]
+  t.equal(str(request.body), str(req.body), match('req.body', req.body))
+  t.equal(str(request.path), str(req.path), match('req.path', req.path))
+  t.equal(str(request.headers), str(req.headers), match(`req.headers`, req.headers))
+  t.equal(str(request.httpMethod), str(req.httpMethod), match('req.httpMethod', req.httpMethod))
+  t.equal(str(request.pathParameters), str(req.pathParameters), match('req.pathParameters', req.pathParameters))
+  t.equal(str(req.queryStringParameters), str(req.queryStringParameters), match('req.queryStringParameters', req.queryStringParameters))
+  t.ok(req.isBase64Encoded, 'req.isBase64Encoded present')
+  teardown()
+})
+
+test('Architect v6: delete /form (JSON)', t => {
+  t.plan(7)
+  let request = reqs.arc6.deleteJson
+  process.env.ARC_CFN = true
+  let verb = 'DELETE'
+  let route = '/form'
+  let handler = invoke({verb, route})
+  let input = {
+    url: url('/form'),
+    body: request.body,
+    headers: request.headers,
+    params: {},
+    isBase64Encoded: true
+  }
+  handler(input, response)
+  let req = lambdaStub.args[0][1]
+  t.equal(str(request.body), str(req.body), match('req.body', req.body))
+  t.equal(str(request.path), str(req.path), match('req.path', req.path))
+  t.equal(str(request.headers), str(req.headers), match(`req.headers`, req.headers))
+  t.equal(str(request.httpMethod), str(req.httpMethod), match('req.httpMethod', req.httpMethod))
+  t.equal(str(request.pathParameters), str(req.pathParameters), match('req.pathParameters', req.pathParameters))
+  t.equal(str(req.queryStringParameters), str(req.queryStringParameters), match('req.queryStringParameters', req.queryStringParameters))
+  t.ok(req.isBase64Encoded, 'req.isBase64Encoded present')
+  teardown()
+})
+
+test('Architect v5: get /', t => {
+  t.plan(6)
+  let request = reqs.arc5.getIndex
+  let verb = 'GET'
+  let route = '/'
+  let handler = invoke({verb, route})
+  let input = {
+    url: url(), // Set by `router` (interpolated, API passes path param)
+    body: {},   // {} set by `body-parser` (Arc 5 == {}, Arc 6 == null)
+    headers,    // Set by requesting client
+    params: {}  // {} set by `router` (Arc 5 == {}, Arc 6 == null)
+  }
+  handler(input, response)
+  // Compare handler-generated request to mock
+  let req = lambdaStub.args[0][1]
+  t.equal(str(request.body), str(req.body), match('req.body', req.body))
+  t.equal(str(request.path), str(req.path), match('req.path', req.path))
+  t.equal(str(request.headers), str(req.headers), match(`req.headers`, req.headers))
+  if (request.httpMethod === request.method)
+    t.equal(req.httpMethod, req.method, match('req.method/httpMethod', req.method))
+  t.equal(str(request.params), str(req.params), match('req.params', req.params))
+  if (str(request.query) === str(req.query))
+    t.equal(str(req.queryStringParameters), str(req.query), match('req.query/queryStringParameters', req.query))
+  lambdaStub.reset()
+})
+
+test('Architect v5: get /?whats=up', t => {
+  t.plan(6)
+  let request = reqs.arc5.getWithQueryString
+  let verb = 'GET'
+  let route = '/'
+  let handler = invoke({verb, route})
+  let input = {
+    url: url('?whats=up'),
+    body: {},
+    headers,
+    params: {}
+  }
+  handler(input, response)
+  let req = lambdaStub.args[0][1]
+  t.equal(str(request.body), str(req.body), match('req.body', req.body))
+  t.equal(str(request.path), str(req.path), match('req.path', req.path))
+  t.equal(str(request.headers), str(req.headers), match(`req.headers`, req.headers))
+  if (request.httpMethod === request.method)
+    t.equal(req.httpMethod, req.method, match('req.method/httpMethod', req.method))
+  t.equal(str(request.params), str(req.params), match('req.params', req.params))
+  if (str(request.query) === str(req.query))
+    t.equal(str(req.queryStringParameters), str(req.query), match('req.query/queryStringParameters', req.query))
+  lambdaStub.reset()
+})
+
+test('Architect v5: get /nature/hiking', t => {
+  t.plan(6)
+  let request = reqs.arc5.getWithParam
+  let verb = 'GET'
+  let route = '/nature/:activities'
+  let handler = invoke({verb, route})
+  let input = {
+    url: url('/nature/hiking'),
+    body: {},
+    headers,
+    params: request.params
+  }
+  handler(input, response)
+  let req = lambdaStub.args[0][1]
+  t.equal(str(request.body), str(req.body), match('req.body', req.body))
+  t.equal(str(request.path), str(req.path), match('req.path', req.path))
+  t.equal(str(request.headers), str(req.headers), match(`req.headers`, req.headers))
+  if (request.httpMethod === request.method)
+    t.equal(req.httpMethod, req.method, match('req.method/httpMethod', req.method))
+  t.equal(str(request.params), str(req.params), match('req.params', req.params))
+  if (str(request.query) === str(req.query))
+    t.equal(str(req.queryStringParameters), str(req.query), match('req.query/queryStringParameters', req.query))
+  lambdaStub.reset()
+})
+
+test('Architect v5: post /form (JSON / form URL-encoded)', t => {
+  t.plan(6)
+  let request = reqs.arc5.post
+  let verb = 'POST'
+  let route = '/form'
+  let handler = invoke({verb, route})
+  let input = {
+    url: url(route),
+    body: request.body,
+    headers,
+    params: {}
+  }
+  handler(input, response)
+  let req = lambdaStub.args[0][1]
+  t.equal(str(request.body), str(req.body), match('req.body', req.body))
+  t.equal(str(request.path), str(req.path), match('req.path', req.path))
+  t.equal(str(request.headers), str(req.headers), match(`req.headers`, req.headers))
+  if (request.httpMethod === request.method)
+    t.equal(req.httpMethod, req.method, match('req.method/httpMethod', req.method))
+  t.equal(str(request.params), str(req.params), match('req.params', req.params))
+  if (str(request.query) === str(req.query))
+    t.equal(str(req.queryStringParameters), str(req.query), match('req.query/queryStringParameters', req.query))
+  lambdaStub.reset()
+})
+
+test('Architect v5: post /form (multipart form data-encoded)', t => {
+  t.plan(6)
+  let request = reqs.arc5.postBinary
+  let verb = 'POST'
+  let route = '/form'
+  let handler = invoke({verb, route})
+  let input = {
+    url: url(route),
+    body: request.body,
+    headers,
+    params: {}
+  }
+  handler(input, response)
+  let req = lambdaStub.args[0][1]
+  t.equal(str(request.body), str(req.body), match('req.body', req.body))
+  t.equal(str(request.path), str(req.path), match('req.path', req.path))
+  t.equal(str(request.headers), str(req.headers), match(`req.headers`, req.headers))
+  if (request.httpMethod === request.method)
+    t.equal(req.httpMethod, req.method, match('req.method/httpMethod', req.method))
+  t.equal(str(request.params), str(req.params), match('req.params', req.params))
+  if (str(request.query) === str(req.query))
+    t.equal(str(req.queryStringParameters), str(req.query), match('req.query/queryStringParameters', req.query))
+  lambdaStub.reset()
+})
+
+test('Architect v5: put /form', t => {
+  t.plan(6)
+  let request = reqs.arc5.put
+  let verb = 'PUT'
+  let route = '/form'
+  let handler = invoke({verb, route})
+  let input = {
+    url: url(route),
+    body: request.body,
+    headers,
+    params: {}
+  }
+  handler(input, response)
+  let req = lambdaStub.args[0][1]
+  t.equal(str(request.body), str(req.body), match('req.body', req.body))
+  t.equal(str(request.path), str(req.path), match('req.path', req.path))
+  t.equal(str(request.headers), str(req.headers), match(`req.headers`, req.headers))
+  if (request.httpMethod === request.method)
+    t.equal(req.httpMethod, req.method, match('req.method/httpMethod', req.method))
+  t.equal(str(request.params), str(req.params), match('req.params', req.params))
+  if (str(request.query) === str(req.query))
+    t.equal(str(req.queryStringParameters), str(req.query), match('req.query/queryStringParameters', req.query))
+  lambdaStub.reset()
+})
+
+test('Architect v5: patch /form', t => {
+  t.plan(6)
+  let request = reqs.arc5.patch
+  let verb = 'PATCH'
+  let route = '/form'
+  let handler = invoke({verb, route})
+  let input = {
+    url: url(route),
+    body: request.body,
+    headers,
+    params: {}
+  }
+  handler(input, response)
+  let req = lambdaStub.args[0][1]
+  t.equal(str(request.body), str(req.body), match('req.body', req.body))
+  t.equal(str(request.path), str(req.path), match('req.path', req.path))
+  t.equal(str(request.headers), str(req.headers), match(`req.headers`, req.headers))
+  if (request.httpMethod === request.method)
+    t.equal(req.httpMethod, req.method, match('req.method/httpMethod', req.method))
+  t.equal(str(request.params), str(req.params), match('req.params', req.params))
+  if (str(request.query) === str(req.query))
+    t.equal(str(req.queryStringParameters), str(req.query), match('req.query/queryStringParameters', req.query))
+  lambdaStub.reset()
+})
+
+test('Architect v5: delete /form', t => {
+  t.plan(6)
+  let request = reqs.arc5.delete
+  let verb = 'DELETE'
+  let route = '/form'
+  let handler = invoke({verb, route})
+  let input = {
+    url: url(route),
+    body: request.body,
+    headers,
+    params: {}
+  }
+  handler(input, response)
+  let req = lambdaStub.args[0][1]
+  t.equal(str(request.body), str(req.body), match('req.body', req.body))
+  t.equal(str(request.path), str(req.path), match('req.path', req.path))
+  t.equal(str(request.headers), str(req.headers), match(`req.headers`, req.headers))
+  if (request.httpMethod === request.method)
+    t.equal(req.httpMethod, req.method, match('req.method/httpMethod', req.method))
+  t.equal(str(request.params), str(req.params), match('req.params', req.params))
+  if (str(request.query) === str(req.query))
+    t.equal(str(req.queryStringParameters), str(req.query), match('req.query/queryStringParameters', req.query))
+  lambdaStub.reset()
+})
