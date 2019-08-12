49c12f1fb3c0c7c70031dfdf5866a9b99d4fc1cc
Ryan Block <rblock@gmail.com>
2019-08-12 15:07:09 -0700

    Tidy up Arc CLI entry

--- src/cli/arc.js
+++ src/cli/arc.js
@@ -1,16 +1,7 @@
-let sandbox = require('../../')
+let sandbox = require('../')
 
-module.exports = function sandboxImplementation(opts) {
-  let findPort = opt=> opt === '-p' || opt === '--port' || opt === 'port'
-  if (opts && opts.some(findPort)) {
-    if (opts.indexOf('-p') >= 0)
-      process.env.PORT = opts[opts.indexOf('-p') + 1]
-    if (opts.indexOf('--port') >= 0)
-      process.env.PORT = opts[opts.indexOf('--port') + 1]
-    if (opts.indexOf('port') >= 0)
-      process.env.PORT = opts[opts.indexOf('port') + 1]
-  }
-  sandbox.start({disableBanner: true}, function _done(err) {
+module.exports = function arcCalling(options) {
+  sandbox.start({disableBanner: true, options}, function _done(err) {
     if (err) {
       console.log(err)
       process.exit(1)
