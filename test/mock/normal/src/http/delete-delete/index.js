ca2b080023d9d14614076e56e1f7a69d7d2a4c4a
Ryan Block <rblock@gmail.com>
2019-08-16 10:50:58 -0700

    Fixes issue where Arc 6 non-GET verbs with bodies were stalling sandbox

---
+++ test/mock/normal/src/http/delete-delete/index.js
@@ -0,0 +1,8 @@
+// Boilerplate Lambda function pulled from AWS
+exports.handler = async (event) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(event),
+  };
+  return response;
+};
