916ccb93b85758efd26f392873c1efd82ad3dd39
Ryan Block <rblock@gmail.com>
2019-09-17 15:12:51 -0700

    Linter friendlier object calls

--- src/db/create-table/_get-attribute-definitions-with-gsi.js
+++ src/db/create-table/_get-attribute-definitions-with-gsi.js
@@ -1,9 +1,9 @@
-var clean = require('./_remove-ttl-and-lambda')
+let clean = require('./_remove-ttl-and-lambda')
 
 // turns {foo:'*String', bar:'**String'}
 // into [{AttributeName:'foo', AttributeType:'S'}, {AttributeName:'bar', AttributeType:'S'}]
 function fixer(obj) {
-  var result = []
+  let result = []
   Object.keys(obj).forEach(AttributeName=> {
     result.push({
       AttributeName,
@@ -15,14 +15,13 @@ function fixer(obj) {
 
 module.exports = function getAttributeDefinitions(attr, name, indexes) {
 
-  var tableName = name.split(/staging-|production-/)[1]
+  let tableName = name.split(/staging-|production-/)[1]
 
   // an array of attributes from indexes [idx:'*String', ts:'**Number}] for example
-  // eslint-disable-next-line
-  var actual = indexes.filter(index=> index.hasOwnProperty(tableName)).map(x=> x[tableName])
+  let actual = indexes.filter(index=> Object.prototype.hasOwnProperty.call(index, tableName)).map(x=> x[tableName])
 
   // interpolates attrs from table definitions and indexes
-  var fixed = actual.map(fixer).concat(fixer(clean(attr))).reduce((a,b)=> a.concat(b))
+  let fixed = actual.map(fixer).concat(fixer(clean(attr))).reduce((a,b)=> a.concat(b))
 
   // an array of [{AttributeName, AttributeType}]
   return fixed
