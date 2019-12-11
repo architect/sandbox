0a9f8f16b12dbe4636241c6b9b6f3c78b2885e7e
Ryan Block <rblock@gmail.com>
2019-12-11 09:22:35 -0800

    Add WebSocket request formatter to emulate AWS event objects (and corresponding tests)

---
+++ src/http/invoke-ws/index.js
@@ -0,0 +1,25 @@
+let invoke = require('../../invoke-lambda')
+let requestFormatter = require('./_req-fmt')
+
+/**
+ * Formats WebSocket request event objects
+ */
+function invokeWebSocketEvent(params, callback) {
+  let {action, body, connectionId, req} = params
+
+  // Only $connect + $disconnect WS invocations have headers
+  if (req && req.headers) {
+    // Coerce API Gateway 'Cookie' from express 'cookie'
+    req.headers.Cookie = req.headers.cookie
+    delete req.headers.cookie
+    if (!req.headers.Cookie) delete req.headers.Cookie
+  }
+
+  // Set up request shape
+  let request = requestFormatter({req, body, connectionId})
+
+  // Run the lambda sig locally
+  invoke(action, request, callback)
+}
+
+module.exports = invokeWebSocketEvent
