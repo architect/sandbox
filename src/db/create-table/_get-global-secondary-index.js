28c24a98dc6998e625062db940a03f4efc4897f9
Ryan Block <rblock@gmail.com>
2020-01-29 19:36:20 -0800

    Same indexing thing, a bit less code

--- src/db/create-table/_get-global-secondary-index.js
+++ src/db/create-table/_get-global-secondary-index.js
@@ -1,8 +1,4 @@
 module.exports = function _getGSI(name, indexes) {
-  return process.env.DEPRECATED? deprec(name, indexes) : gsi(name, indexes)
-}
-
-function gsi(name, indexes) {
   let tableName = name.split(/staging-|production-/)[1]
   let actual = indexes.filter(index=> Object.prototype.hasOwnProperty.call(index, tableName))
   if (actual.length >= 1) {
@@ -11,59 +7,25 @@ function gsi(name, indexes) {
       if (keys.length > 2 || keys.length < 1) {
         throw Error(`@indexes ${tableName} has wrong number of keys`)
       }
-      let hasOne = keys.length === 1
-      let hasTwo = keys.length === 2
-      let IndexName = hasOne? `${keys[0]}-index` : `${keys[0]}-${keys[1]}-index`
-      // always add the HASH key (partition)
-      let KeySchema = [{
-        AttributeName: keys[0],
-        KeyType: 'HASH'
-      }]
-      // maybe add the RANGE key (sort)
-      if (hasTwo) {
-        KeySchema.push({
-          AttributeName: keys[1],
-          KeyType: 'RANGE'
-        })
-      }
-      let params = {
-        IndexName,
-        KeySchema,
-        Projection: {
-          ProjectionType: 'ALL'
-        },
-        ProvisionedThroughput: {
-          ReadCapacityUnits: 5,
-          WriteCapacityUnits: 5
-        }
-      }
-      return params
-    })
-  }
-  else {
-    return false
-  }
-}
+      let hasOneKey = keys.length === 1
+      let hasTwoKeys = keys.length === 2
 
-function deprec(name, indexes) {
-  let tableName = name.split(/staging-|production-/)[1]
-  let actual = indexes.filter(index=> Object.prototype.hasOwnProperty.call(index, tableName))
-  if (actual.length >= 1) {
-    return actual.map(idx=> {
-      let keys = Object.keys(idx[tableName])
-      if (keys.length > 2 || keys.length < 1) {
-        throw Error(`@indexes ${tableName} has wrong number of keys`)
-      }
-      let hasOne = keys.length === 1
-      let hasTwo = keys.length === 2
-      let IndexName = hasOne? `${name}-${keys[0]}-index` : `${name}-${keys[0]}-${keys[1]}-index`
+      let deprecated = process.env.DEPRECATED
+      let singleKey = deprecated
+        ? `${name}-${keys[0]}-index` // Old school index naming
+        : `${keys[0]}-index` // New school index naming
+      let multiKey = deprecated
+        ? `${name}-${keys[0]}-${keys[1]}-index`
+        : `${keys[0]}-${keys[1]}-index`
+
+      let IndexName = hasOneKey ? singleKey : multiKey
       // always add the HASH key (partition)
       let KeySchema = [{
         AttributeName: keys[0],
         KeyType: 'HASH'
       }]
       // maybe add the RANGE key (sort)
-      if (hasTwo) {
+      if (hasTwoKeys) {
         KeySchema.push({
           AttributeName: keys[1],
           KeyType: 'RANGE'
