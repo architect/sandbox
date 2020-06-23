d94ee3f447af04d0fbab9efeed7d59a9f1f2bd4e
Fil Maj <maj.fil@gmail.com>
2020-06-23 13:40:24 -0400

    when cleaning tables attributes, ignore encrypt param. added unit tests for the clean module for this. (#340)

---
+++ test/unit/src/db/create-table/_remove-ttl-and-lambda-test.js
@@ -0,0 +1,30 @@
+let clean = require('../../../../../src/db/create-table/_remove-ttl-and-lambda')
+let test = require('tape')
+
+test('_remove-ttl-and-lambda should ignore params with a value of TTL', t => {
+  t.plan(2)
+  let cleaned = clean({key:'value',_ttl:'TTL'})
+  t.equals(cleaned.key, 'value', 'did not remove legit value')
+  t.notOk(cleaned._ttl, 'removed TTL param')
+});
+
+test('_remove-ttl-and-lambda should ignore stream param', t => {
+  t.plan(2)
+  let cleaned = clean({key:'value',stream:'yes'})
+  t.equals(cleaned.key, 'value', 'did not remove legit value')
+  t.notOk(cleaned.stream, 'removed stream param')
+});
+
+test('_remove-ttl-and-lambda should ignore params with a value of Lambda', t => {
+  t.plan(2)
+  let cleaned = clean({key:'value',wut:'Lambda'})
+  t.equals(cleaned.key, 'value', 'did not remove legit value')
+  t.notOk(cleaned.Lambda, 'removed Lambda param')
+});
+
+test('_remove-ttl-and-lambda should ignore encrypt param', t => {
+  t.plan(2)
+  let cleaned = clean({key:'value',encrypt:true})
+  t.equals(cleaned.key, 'value', 'did not remove legit value')
+  t.notOk(cleaned.encrypt, 'removed encrypt param')
+});
