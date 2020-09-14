bb92923a3f7af2364a0eff8d289051674dbfbf1e
Ryan Block <200964+ryanblock@users.noreply.github.com>
2020-09-14 14:29:59 -0700

    Fix arc config (#457)
    * Un-break Lambda invocation if an object is present in .arc-config
    
    * Update deps and changelog
    
    * Update changelog

--- src/invoke-lambda/get-config.js
+++ src/invoke-lambda/get-config.js
@@ -12,7 +12,7 @@ module.exports = function readConfigFor (cwd, callback) {
     }
     else {
       let arc = parse(data.toString())
-      let awsRuntime = arc.aws.find(tuple => tuple.includes('runtime'))
+      let awsRuntime = arc.aws.find(tuple => Array.isArray(tuple) && tuple.includes('runtime'))
       let runtime = awsRuntime ? awsRuntime[1] : 'nodejs12.x'
       let timeout = getTimeout(arc)
       callback(null, { runtime, timeout })
@@ -26,7 +26,7 @@ module.exports = function readConfigFor (cwd, callback) {
 function getTimeout (arc) {
   let timeout = process.env.SANDBOX_TIMEOUT && process.env.SANDBOX_TIMEOUT * 1000
   timeout = timeout || 5 * 1000 // 5 seconds in milliseconds
-  let ttl = arc && arc.aws.find(tuple => tuple.includes('timeout'))
+  let ttl = arc && arc.aws.find(tuple => Array.isArray(tuple) && tuple.includes('timeout'))
   // valid ttl is between 3 and 900 seconds (15 minutes)
   if (ttl && ttl[1] > 3 && ttl[1] <= 900) {
     timeout = ttl[1] * 1000 // ttl seconds in milliseconds
