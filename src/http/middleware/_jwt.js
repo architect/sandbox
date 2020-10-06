7bec250c1906957d5b12ef13d46d5faaec242667
Ankcorn <thomasankcorn@gmail.com>
2020-10-06 22:48:06 +0100

    handle invalid jwt's correctly

--- src/http/middleware/_jwt.js
+++ src/http/middleware/_jwt.js
@@ -1,15 +1,20 @@
+
+// This lib is quite large at 339kb is there a better and lighter jwt decoder we could use
 const jsonwebtoken = require('jsonwebtoken')
 
 module.exports = function (arc) {
-
 	return function jwt(req, res, next) {
 		const route = arc.http.find(route => route[0] === req.method.toLowerCase() && route[1] === req.url);
 		if (route.length > 2) {
 			console.log(req.headers)
-			if (req.headers.authorization) {
+			try {
+				if (!req.headers.authorization) {
+					throw Error('Unauthorized');
+				}
+				// This operation is not secure do not do this in production
 				const tokenValues = jsonwebtoken.decode(req.headers.authorization.split(' ')[1]);
 				req.authorizer = { jwt: { claims: tokenValues } }
-			} else {
+			} catch(e) {
 				res.writeHead(401)
 				res.end('{"message":"Unauthorized"}')
 				return
