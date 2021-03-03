293229fd408a0215d69ff92290425fa92965d1e9
filmaj <maj.fil@gmail.com>
2021-03-02 19:26:13 -0500

    bumping inventory up to support plugins, uncommenting plugin-specific tests, fixing up tests.

--- test/integration/plugin-service-async-test.js
+++ test/integration/plugin-service-async-test.js
@@ -1,21 +1,37 @@
 let { join } = require('path')
 let fs = require('fs')
-let os = require('os')
 let test = require('tape')
 let sut = join(process.cwd(), 'src')
 let sandbox = require(sut)
 let cwd = process.cwd()
 let mock = join(__dirname, '..', 'mock')
+let file = join(process.cwd(), 'test', 'mock', 'plugins-async', 'asyncplugin.test')
 
-// TODO: unskip the tests once new inventory version is bumped
-test.skip('Set up env', t => {
+function exists (f) {
+  let callback
+  let promise = new Promise(function (res, rej) {
+    callback = function (err) {
+      if (err) {
+        if (err.code === 'ENOENT') res(false)
+        else rej(err)
+      }
+      else {
+        res(true)
+      }
+    }
+  })
+  fs.stat(f, callback)
+  return promise
+}
+
+test('Set up env', t => {
   t.plan(2)
   t.ok(sandbox, 'Sandbox is present')
   t.ok(sandbox.start, 'sandbox.start module is present')
   process.chdir(join(mock, 'plugins-async'))
 })
 
-test.skip('Async sandbox.start', async t => {
+test('Async sandbox.start', async t => {
   t.plan(1)
   try {
     await sandbox.start({ quiet: true })
@@ -23,10 +39,17 @@ test.skip('Async sandbox.start', async t => {
   catch (err) {
     t.fail(err)
   }
-  t.ok(fs.existsSync(join(os.tmpdir(), 'asyncplugin.test')), 'plugin sandbox service start executed successfully')
+  let result
+  try {
+    result = await exists(file)
+  }
+  catch (err) {
+    t.fail(err)
+  }
+  t.ok(result, `plugin sandbox service start executed successfully (created ${file})`)
 })
 
-test.skip('Async sandbox.end', async t => {
+test('Async sandbox.end', async t => {
   t.plan(2)
   try {
     await sandbox.end()
@@ -34,7 +57,14 @@ test.skip('Async sandbox.end', async t => {
   catch (err) {
     t.fail(err)
   }
-  t.notOk(fs.existsSync(join(os.tmpdir(), 'asyncplugin.test')), 'plugin sandbox service end executed successfully')
+  let result
+  try {
+    result = await exists(file)
+  }
+  catch (err) {
+    t.fail(err)
+  }
+  t.notOk(result, `plugin sandbox service end executed successfully (removed ${file})`)
   process.chdir(cwd)
   t.equal(process.cwd(), cwd, 'Switched back to original working dir')
 })
