d53250f42f49f7a7af754c700731a8c5bb352c61
filmaj <maj.fil@gmail.com>
2021-04-16 10:59:30 -0400

    wrap the try catch in service discovery SSM mock response generation around parsing the SSM payload only. use cloudformation stack name and not app name as root SSM path segment. infer cloudformation stack from request body.

--- src/arc/_ssm/index.js
+++ src/arc/_ssm/index.js
@@ -5,33 +5,33 @@ module.exports = function _ssm (params, req, res) {
 
   // Populate services: a rare inv mutation, necessary because this is an artifact of the current inventory
   services(inventory)
-  let { app, _serviceDiscovery } = inventory.inv
+  let { _serviceDiscovery } = inventory.inv
 
-  let message
+  let message, stack
+  let Parameters = []
   try {
     message = JSON.parse(body)
-    let Parameters = []
-    let type = message.Path.split('/').filter(Boolean)[1]
-    if (_serviceDiscovery[type]) {
-      Object.entries(_serviceDiscovery[type]).forEach(([ key, Value ]) => {
-        Parameters.push({
-          Name: `/${app}/${type}/${key}`,
-          Type: 'String',
-          Value,
-          Version: 1,
-          LastModifiedDate: new Date(Date.now()).toISOString(),
-          ARN: 'Architect Sandbox'
-        })
-      })
-    }
-    res.statusCode = 200
-    res.setHeader('content-type', 'application/json')
-    res.end(JSON.stringify({ Parameters }))
-    return
+    stack = message.Path.split('/').filter(Boolean)[0] // cloudformation stack name
   }
   catch (e) {
     res.statusCode = 400
     res.end('Sandbox service discovery exception parsing request body')
     return
   }
+  Object.entries(_serviceDiscovery).forEach(([ type, map ]) => {
+    Object.entries(map).forEach(([ key, Value ]) => {
+      Parameters.push({
+        Name: `/${stack}/${type}/${key}`,
+        Type: 'String',
+        Value,
+        Version: 1,
+        LastModifiedDate: new Date(Date.now()).toISOString(),
+        ARN: 'Architect Sandbox'
+      })
+    })
+  })
+  res.statusCode = 200
+  res.setHeader('content-type', 'application/json')
+  res.end(JSON.stringify({ Parameters }))
+  return
 }
