079ed2dd9d743e2d43712303b16a37c155db0d8d
filmaj <maj.fil@gmail.com>
2021-04-27 13:17:10 -0400

    adding arc ssm services module unit tests

---
+++ test/unit/src/arc/_ssm/services-test.js
@@ -0,0 +1,22 @@
+let test = require('tape')
+let services = require('../../../../../src/arc/_ssm/_services')
+
+test('should not mutate inventory if serviceDiscovery property already exists', t => {
+  t.plan(1)
+  let inventory = { inv: { _serviceDiscovery: {} } }
+  let invFingerprint = JSON.stringify(inventory)
+  services(inventory)
+  t.equals(JSON.stringify(inventory), invFingerprint, 'inventory not modified')
+})
+test('should populate serviceDiscovery property with plugin variables', t => {
+  t.plan(3)
+  let inventory = { inv: { app: 'testapp', _project: { plugins: {
+    pluginOne: { variables: () => ({ oneVar: 'yep' }) },
+    pluginTwo: { variables: () => ({ twoVar: 'yup' }) }
+  } } } }
+  services(inventory)
+  let svcs = inventory.inv._serviceDiscovery
+  t.ok(svcs, '_serviceDiscovery property created')
+  t.equals(svcs.pluginOne.oneVar, 'yep', 'first plugin variable created')
+  t.equals(svcs.pluginTwo.twoVar, 'yup', 'second plugin variable created')
+})
