1c20ee6e7d661eb10198374ae4354ffa9dbcf494
Martin Hicks <hello@martinhicks.net>
2021-05-17 11:05:30 +0100

    enable loading of a custom tsconfig using the --config runtime flag. Custom config will check the vendor/views path and the cwd of invoked function for `deno.tsconfig.json`

--- src/invoke-lambda/run-in-deno.js
+++ src/invoke-lambda/run-in-deno.js
@@ -1,8 +1,22 @@
 let path = require('path')
 let spawn = require('./spawn')
+let fs = require('fs')
 
 module.exports = function runInNode (options, request, timeout, callback) {
+
+  let denoGlobalConfigPath = path.join(options.cwd, 'vendor', 'views', 'deno.tsconfig.json')
+  let denoFunctionConfigPath = path.join(options.cwd, 'deno.tsconfig.json')
+  let denoConfig = '';
+
+  if(fs.existsSync(denoGlobalConfigPath)) {
+    denoConfig = `--config ${denoGlobalConfigPath}`
+  }
+
+  if(fs.existsSync(denoFunctionConfigPath)) {
+    denoConfig = `--config ${denoFunctionConfigPath}`
+  }
+
   spawn('deno', [
-    'run', '-A', '--unstable', '--reload', path.join(__dirname, 'runtimes', 'deno.js')
+    'run', '-A', '--unstable',  '--reload', `${denoConfig}`, path.join(__dirname, 'runtimes', 'deno.js')
   ], options, request, timeout, callback)
 }
