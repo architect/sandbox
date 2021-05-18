1701583e4086a7024266df785850148f665c46bb
Ryan Block <rblock@gmail.com>
2021-05-18 09:51:13 -0700

    Load runtime scripts just once

--- src/invoke-lambda/run-in-python.js
+++ src/invoke-lambda/run-in-python.js
@@ -1,21 +1,15 @@
-let { readFile } = require('fs')
-let { join } = require('path')
+let load = require('./runtimes/_loader')
 let spawn = require('./spawn')
 
 module.exports = function runInPython (params, callback) {
-  let python = join(__dirname, 'runtimes', 'python.py')
-  let py = process.platform === 'win32' ? 'python' : 'python3'
-  readFile(python, 'utf8', function done (err, data) {
-    if (err) callback(err)
-    else {
-      // Windows `python -c` doesn't like multi-liners, so serialize script
-      data = data.toString().split('\n').filter(l => l.trim() !== '').join(';')
-      let script = `"${data}"`
-      spawn({
-        command: py,
-        args: [ '-c', script ],
-        ...params,
-      }, callback)
-    }
-  })
+  let { python } = load()
+  // Windows `python -c` doesn't like multi-liners, so serialize script
+  let command = process.platform === 'win32' ? 'python' : 'python3'
+  let script = python.split('\n').filter(l => l.trim() !== '').join(';')
+  script = `"${script}"`
+  spawn({
+    command,
+    args: [ '-c', script ],
+    ...params,
+  }, callback)
 }
--- src/invoke-lambda/run-in-ruby.js
+++ src/invoke-lambda/run-in-ruby.js
@@ -1,23 +1,16 @@
-let { readFile } = require('fs')
-let { join } = require('path')
+let load = require('./runtimes/_loader')
 let spawn = require('./spawn')
 
 module.exports = function runInRuby (params, callback) {
-  let ruby = join(__dirname, 'runtimes', 'ruby.rb')
-  readFile(ruby, 'utf8', function done (err, data) {
-    if (err) callback(err)
-    else {
-      let script = data.toString()
-      let args = script
-        .split('\n')
-        .filter(Boolean)
-        .map(line => [ '-e', `"${line}"` ])
-        .reduce((a, b) => a.concat(b))
-      spawn({
-        command: 'ruby',
-        args,
-        ...params,
-      }, callback)
-    }
-  })
+  let { ruby } = load()
+  let args = ruby
+    .split('\n')
+    .filter(Boolean)
+    .map(line => [ '-e', `"${line}"` ])
+    .reduce((a, b) => a.concat(b))
+  spawn({
+    command: 'ruby',
+    args,
+    ...params,
+  }, callback)
 }
