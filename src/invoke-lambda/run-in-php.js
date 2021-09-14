adea7df6460f26f9e74b13bdb410759b90893752
Martin Hicks <hello@martinhicks.net>
2021-09-14 20:14:57 +0100

    cleanup

--- src/invoke-lambda/run-in-php.js
+++ src/invoke-lambda/run-in-php.js
@@ -3,11 +3,9 @@ let { join } = require('path')
 
 module.exports = function runInPhp (params, callback) {
   
-    // PHP can't easily be invoked with a string of code like python / ruby
-
-    let runtimePath = join(__dirname, "runtimes", "php.php")
+  // PHP can't easily be invoked with a string of code like python / ruby
+  let runtimePath = join(__dirname, "runtimes", "php.php")
     
-
   spawn({
     command: 'php',
     args: [ runtimePath],
