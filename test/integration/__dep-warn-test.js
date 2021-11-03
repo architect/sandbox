27ca09101de6e91e587b8c0c320693f588005c5b
Ryan Block <rblock@gmail.com>
2021-11-03 08:37:57 -0700

    really

--- test/integration/__dep-warn-test.js
+++ test/integration/__dep-warn-test.js
@@ -1,6 +1,6 @@
 let { join } = require('path')
-let { existsSync, mkdirSync, readdirSync } = require('fs')
-let { execSync } = require('child_process')
+let { existsSync, mkdirSync, rmSync } = require('fs')
+// let { execSync } = require('child_process')
 let test = require('tape')
 let sut = join(process.cwd(), 'src')
 let sandbox = require(sut)
@@ -19,14 +19,14 @@ let stdout = process.stdout.write
 
 function prep (t, copying) {
   try {
-    // rmSync(tmp, { recursive: true, force: true, maxRetries: 100 })
+    rmSync(tmp, { recursive: true, force: true, maxRetries: 100 })
+    // if (existsSync(tmp)) {
+    //   let cmd = process.platform.startsWith('win') ? 'rmdir /s /q tmp' : 'rm -rf tmp'
+    //   let result = execSync(cmd, { cwd: mock, shell: true })
+    //   console.log(`result:`, result.toString())
+    // }
     if (existsSync(tmp)) {
-      let cmd = process.platform.startsWith('win') ? 'rmdir /s /q tmp' : 'rm -rf tmp'
-      let result = execSync(cmd, { cwd: mock, shell: true })
-      console.log(`result:`, result.toString())
-    }
-    if (existsSync(tmp)) {
-      console.log(`tmp dir contents:`, readdirSync(tmp))
+      // console.log(`tmp dir contents:`, readdirSync(tmp))
       t.fail(`${tmp} should not exist`)
       process.exit(1)
     }
@@ -357,8 +357,13 @@ function runTests (runType, t ) {
     })
   })
 
-  t.test(`[Dependency warnings] Teardown`, t => {
-    prep(t)
+  t.test(`[Dependency warnings (shared - packages in shared + Lambdas) ${mode}] Shut down Sandbox`, t => {
     shutdown[runType](t)
+  })
+
+  t.test(`[Dependency warnings] Teardown`, t => {
+    t.plan(1)
+    prep(t)
+    t.pass('Done')
   })
 }
