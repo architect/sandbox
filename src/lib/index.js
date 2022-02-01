92e9586bad4d76419ab6e4b0c95b3f6999b503e3
Ryan Block <rblock@gmail.com>
2022-02-01 09:27:30 -0800

    Update references to recently moved internal methods

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,15 +1,9 @@
-let checkRuntimes = require('./check-runtimes')
-let env = require('./env')
 let getFlags = require('./flags')
-let maybeHydrate = require('./maybe-hydrate')
 let runtimeEval = require('./runtime-eval')
 let template = require('./template')
 
 module.exports = {
-  checkRuntimes,
-  env,
   getFlags,
-  maybeHydrate,
   runtimeEval,
   template
 }
