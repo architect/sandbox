89352d207d20e4e40102d98b25bd1dc0b426b218
Ryan Block <rblock@gmail.com>
2022-02-03 04:35:01 -0800

    Fix startup logging order and printing predicates

--- src/sandbox/print-status.js
+++ src/sandbox/print-status.js
@@ -11,50 +11,52 @@ module.exports = function prettyPrint (params, start, callback) {
   let { cwd, inventory, ports, restart, update } = params
   let { http, ws } = inventory.inv
 
-  if (restart || (!http && !ws)) {
+  if (restart) {
     return callback()
   }
 
   /**
    * All the HTTP/WS things
    */
-  let { apiType } = httpConfig(params)
-  let padL = str => str.padStart(7)
-  let padR = str => str.padEnd(45, '.')
-  let strip = str => str.replace(cwd, '').substr(1)
-
-  if (http) {
-    let folder = `${inventory.inv.static.folder}${sep}`
-    let msgs = {
-      rest: 'REST API mode / Lambda proxy',
-      http: 'HTTP API mode / Lambda proxy v2.0 format',
-      httpv2: 'HTTP API mode / Lambda proxy v2.0 format',
-      httpv1: 'HTTP API mode / Lambda proxy v1.0 format',
-    }
-    let andWs = ws ? '& @ws ' : ''
-    update.done(`Available @http (${msgs[apiType]}) ${andWs}routes`)
-    http.forEach(({ method, path, src, arcStaticAssetProxy }) => {
-      let routeMethod = chalk.grey(padL(method))
-      let routePath = chalk.cyan(path) + ' '
-      let lambdaPath = strip(src)
-      let lambda = chalk.grey(arcStaticAssetProxy ? folder : lambdaPath)
-      update.raw(chalk.grey(`${routeMethod} ${padR(routePath)} ${lambda}`))
-    })
-  }
-
-  if (ws) {
-    ws.forEach(({ name, src }) => {
-      let type = chalk.grey(padL('ws'))
-      let routeName = chalk.cyan(name) + ' '
-      let lambdaPath = strip(src)
-      let lambda = chalk.grey(lambdaPath)
-      update.raw(chalk.grey(`${type} ${padR(routeName)} ${lambda}`))
-    })
-  }
-
   if (http || ws) {
-    let link = chalk.green.bold.underline(`http://localhost:${ports.http}\n`)
-    update.raw(`\n    ${link}`)
+    let { apiType } = httpConfig(params)
+    let padL = str => str.padStart(7)
+    let padR = str => str.padEnd(45, '.')
+    let strip = str => str.replace(cwd, '').substr(1)
+
+    if (http) {
+      let folder = `${inventory.inv.static.folder}${sep}`
+      let msgs = {
+        rest: 'REST API mode / Lambda proxy',
+        http: 'HTTP API mode / Lambda proxy v2.0 format',
+        httpv2: 'HTTP API mode / Lambda proxy v2.0 format',
+        httpv1: 'HTTP API mode / Lambda proxy v1.0 format',
+      }
+      let andWs = ws ? '& @ws ' : ''
+      update.done(`Available @http (${msgs[apiType]}) ${andWs}routes`)
+      http.forEach(({ method, path, src, arcStaticAssetProxy }) => {
+        let routeMethod = chalk.grey(padL(method))
+        let routePath = chalk.cyan(path) + ' '
+        let lambdaPath = strip(src)
+        let lambda = chalk.grey(arcStaticAssetProxy ? folder : lambdaPath)
+        update.raw(chalk.grey(`${routeMethod} ${padR(routePath)} ${lambda}`))
+      })
+    }
+
+    if (ws) {
+      ws.forEach(({ name, src }) => {
+        let type = chalk.grey(padL('ws'))
+        let routeName = chalk.cyan(name) + ' '
+        let lambdaPath = strip(src)
+        let lambda = chalk.grey(lambdaPath)
+        update.raw(chalk.grey(`${type} ${padR(routeName)} ${lambda}`))
+      })
+    }
+
+    if (http || ws) {
+      let link = chalk.green.bold.underline(`http://localhost:${ports.http}\n`)
+      update.raw(`\n    ${link}`)
+    }
   }
 
   /**
