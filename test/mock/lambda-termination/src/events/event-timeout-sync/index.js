d489a2d24a4c1d266c3833b62e2286f69c555e1d
Ryan Block <rblock@gmail.com>
2023-02-07 10:23:38 -0800

    Reduce likelihood of false negative tests on slow file writes in Win + Mac CI matrix

--- test/mock/lambda-termination/src/events/event-timeout-sync/index.js
+++ test/mock/lambda-termination/src/events/event-timeout-sync/index.js
@@ -4,8 +4,7 @@ let { join } = require('path')
 exports.handler = (event, context, callback) => {
   event = JSON.parse(event.Records[0].Sns.Message)
   let pathToFile = event.path
-
-  let timeout = 1250 // this event is configured to time out after 1s (see config.arc), so lets setTimeout longer
+  let timeout = 1250 // this event is configured to time out after 1s (see config.arc), so let's setTimeout longer
   console.log(`event-timeout-sync will write to ${pathToFile} in ${timeout}ms...`)
   setTimeout(() => {
     // this should never execute as timeout is set to 1 sec in config.arc
