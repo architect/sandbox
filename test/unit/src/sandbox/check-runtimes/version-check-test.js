d56980428d7e85d37bde181ab8e11c2466377112
Ryan Block <rblock@gmail.com>
2023-04-07 18:48:32 -0700

    Update deps, tests for `nodejs12.x` deprecation

--- test/unit/src/sandbox/check-runtimes/version-check-test.js
+++ test/unit/src/sandbox/check-runtimes/version-check-test.js
@@ -37,16 +37,16 @@ test('Project global runtime config', async t => {
   t.ok(result[1].includes('aliased to node'), 'Reported issue with alias')
 
   // Minor
-  rawArc = basic + '\n@aws\nruntime nodejs12.x'
+  rawArc = basic + '\n@aws\nruntime nodejs16.x'
   inventory = await _inventory({ rawArc })
-  localRuntimes = { node: '12.1.0' }
+  localRuntimes = { node: '16.1.0' }
   result = versionCheck({ cwd, inventory, localRuntimes })
   t.notOk(result, 'Compatible runtime configuration did not return any issues')
 
   // Major
-  rawArc = basic + '\n@aws\nruntime nodejs12.x'
+  rawArc = basic + '\n@aws\nruntime nodejs18.x'
   inventory = await _inventory({ rawArc })
-  localRuntimes = { node: '10.0.0' }
+  localRuntimes = { node: '16.0.0' }
   result = versionCheck({ cwd, inventory, localRuntimes })
   console.log(result[1])
   t.ok(result[1].includes('Project global runtime'), 'Reported issue with project global runtime')
@@ -121,18 +121,18 @@ test('Per-Lambda runtime config', async t => {
 
   // Minor
   inventory = await _inventory({ rawArc })
-  inventory.inv.http[0].config.runtime = 'nodejs12.x'
-  localRuntimes = { node: '12.1.0' }
+  inventory.inv.http[0].config.runtime = 'nodejs14.x'
+  localRuntimes = { node: '14.1.0' }
   result = versionCheck({ cwd, inventory, localRuntimes })
   t.notOk(result, 'Compatible runtime configuration did not return any issues')
 
   // Major
   inventory = await _inventory({ rawArc })
-  inventory.inv.http[0].config.runtime = 'nodejs14.x'
-  localRuntimes = { node: '12.0.0' }
+  inventory.inv.http[0].config.runtime = 'nodejs18.x'
+  localRuntimes = { node: '16.0.0' }
   result = versionCheck({ cwd, inventory, localRuntimes })
   console.log(result[1])
-  t.ok(result[1].includes('nodejs14.x'), 'Reported issue with Lambda runtime')
+  t.ok(result[1].includes('nodejs18.x'), 'Reported issue with Lambda runtime')
 
   // Minor
   inventory = await _inventory({ rawArc })
