b7a031b85f3c37cf7b6c888eb367a633c356d800
filmaj <maj.fil@gmail.com>
2025-07-01 20:10:23 -0400

    add test fixtures

---
+++ test/mock/seed-data/mjs/sandbox-seed.mjs
@@ -0,0 +1,24 @@
+export default {
+  stuff: [
+    {
+      id: 'foo',
+      thing: 'bar'
+    },
+    {
+      id: 'fiz',
+      thing: 'buz'
+    }
+  ],
+  things: [
+    {
+      id: 'foo',
+      sort: 123,
+      thing: 'bar'
+    },
+    {
+      id: 'foo',
+      sort: 456,
+      thing: 'buz'
+    }
+  ]
+}
